98bdaf3d9cb41fcd3adadb29fc5add5917039f5d
Chaoyi-He1 <chaoyi_he@tamu.edu>
2023-03-08 21:40:41 -0600

    Update

---
+++ Support_Vector_Regression/model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+
+class SVR_net(nn.Module):
+    def __init__(self, cfg: dict):
+        super(SVR_net, self).__init__()
+        self.linear = nn.Linear(in_features=cfg["num_feature"], out_features=cfg["num_feature"], bias=cfg["bias"])
+
+    def forward(self, inputs: Tensor):
+        return self.linear(inputs)
