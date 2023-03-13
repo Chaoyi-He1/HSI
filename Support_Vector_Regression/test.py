d267eab1af5fb082818d8b72565e8874604220f0
Chaoyi-He1 <chaoyi_he@tamu.edu>
2023-03-13 17:02:14 -0500

    Update

--- Support_Vector_Regression/test.py
+++ Support_Vector_Regression/test.py
@@ -1,5 +1,6 @@
 import neo
 
 
-r = neo.io.Spike2IO(filename='/path/to/file.hsd')
+r = neo.io.Spike2IO(filename='rgb20190528_180044_6684_json/20190528_180044_6684.hsd')
 img = r.read_block()
+print(img)
