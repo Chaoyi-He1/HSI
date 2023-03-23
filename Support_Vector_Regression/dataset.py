e81c899158a5f6559a31a83ed959921fc5cd0894
Chaoyi-He1 <chaoyi_he@tamu.edu>
2023-03-23 18:15:23 -0500

    Update

--- Support_Vector_Regression/dataset.py
+++ Support_Vector_Regression/dataset.py
@@ -2,57 +2,66 @@ from PIL import Image
 import torch
 import os
 import scipy.io as sio
-import neo
+import numpy as np
 from pathlib import Path
 from torch.utils.data import Dataset
 
 
-class MyDataSet(Dataset):
-    def __init__(self, images_path: str, filter_path: str):
-        self.images_path = images_path
-        self.filter_path = filter_path
-        try:
-            self.images_path = str(Path(self.images_path))
-            # parent = str(Path(path).parent) + os.sep
-            if os.path.isfile(self.images_path):  # file
-                # Read the data from "data/my_train.txt" or "data/val_data.txt"，read each line as a file
-                with open(self.images_path, "r") as f:
-                    f = f.read().splitlines()
-            else:
-                raise Exception("%s does not exist" % self.images_path)
-            self.img_files = [x for x in f]
-            # Prevent different systems from sorting differently, resulting in differences in shape files
-            self.img_files.sort()
-        except Exception as e:
-            raise FileNotFoundError("Error loading data from {}. {}".format(self.images_path, e))
+class HSI_Segmentation(Dataset):
+    def __init__(self, data_path: str = "", filter_path: str = "", img_type: str = "raw", transforms=None):
+        """
+        Parameters:
+            data_path: the path of the "HSI Dataset folder"
+            label_type: can be either "gray" or "viz"
+            img_type: can be either "OSP" or "PCA"
+            transforms: augmentation methods for images.
+        """
+        super(HSI_Segmentation, self).__init__()
+        assert os.path.isdir(data_path), "path '{}' does not exist.".format(data_path)
+        self.img_folder_list = os.listdir(data_path)
+        self.img_type = img_type
 
-        try:
-            self.filter_path = str(Path(self.filter_path))
-            if os.path.isfile(self.filter_path):
-                self.mat_contents = sio.loadmat(self.filter_path)
-            else:
-                raise Exception("%s does not exist" % self.images_path)
-            self.filter_respond = torch.as_tensor(self.mat_contents["responsivity"])
-        except Exception as e:
-            raise FileNotFoundError("Error loading data from {}. {}".format(self.images_path, e))
+        if img_type != 'rgb':
+            self.img_files = [os.path.join(data_path, img_folder, file)
+                              for img_folder in self.img_folder_list
+                              for file in os.listdir(os.path.join(data_path, img_folder))
+                              if os.path.splitext(file)[-1].lower() == ".mat" and img_type in file]
+        else:
+            self.img_files = [os.path.join(data_path, img_folder, file)
+                              for img_folder in self.img_folder_list
+                              for file in os.listdir(os.path.join(data_path, img_folder))
+                              if os.path.splitext(file)[-1].lower() == ".png" and img_type in file]
+
+        self.img_files.sort()
+        self.filter_file = filter_path
+        self.filters = sio.loadmat(self.filter_file)["responsivity"].astype(np.float)  # shape: (89, 71)
+
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is the image segmentation.
+        """
+        img = sio.loadmat(self.img_files[index])["filtered_img"].astype(np.float16) \
+            if self.img_type != "rgb" else Image.open(self.img_files[index])
+        # img = np.ascontiguousarray(img.transpose(2, 0, 1))
+        # img = (img - np.min(img)) * 255 / np.max(img)
+        # img = Image.fromarray(img)
+
+        if self.transforms is not None:
+            img, target = self.transforms(img)
+
+        return img, self.filters
 
     def __len__(self):
         return len(self.img_files)
 
-    def __getitem__(self, item):
-        r = neo.io.Spike2IO(filename=self.img_files[item])
-        img = r.read_block()
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        return img
-
     @staticmethod
     def collate_fn(batch):
-        images, labels = tuple(zip(*batch))
-
-        images = torch.stack(images, dim=0)
-        labels = torch.as_tensor(labels)
-        return images, labels
-
+        images, filters = list(zip(*batch))
+        flattened_imgs = images.permute(0, 2, 3, 1).flatten(0, 2).contiguous()
+        return flattened_imgs, filters.repeat(flattened_imgs.shape[0], 1, 1)
