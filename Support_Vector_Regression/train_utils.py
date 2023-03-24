b638536e960efeb66d48eb55a8e03ff1fde9e61b
Chaoyi-He1 <chaoyi_he@tamu.edu>
2023-03-24 14:36:28 -0500

    Update

--- Support_Vector_Regression/train_utils.py
+++ Support_Vector_Regression/train_utils.py
@@ -7,18 +7,20 @@ import torch
 
 def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, print_freq: int):
+                    device: torch.device, epoch: int, print_freq: int, scaler=None):
     model.train()
     criterion.train()
     total_time = time.time()
+    ave_loss = torch.tensor(0.).to(device)
     for i, (samples, targets) in enumerate(data_loader):
         samples = samples.to(device)
         targets = targets.to(device)
         iter_time = time.time()
-        outputs = model(samples)
-        iter_time = time.time() - iter_time
+        with torch.cuda.amp.autocast(enabled=scaler is not None):
+            outputs = model(samples)
+            loss = criterion(outputs, targets)
+            ave_loss += loss
 
-        loss = criterion(outputs, targets)
         if not torch.isfinite(loss):
             print("Loss is {}, stopping training".format(loss))
             sys.exit(1)
@@ -29,6 +31,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         optimizer.step()
 
         if i % print_freq == 0 or i == len(data_loader) - 1:
+            iter_time = time.time() - iter_time
             log_msg = "; ".join([
                 'Epoch: [{}]'.format(epoch),
                 '[{0' + ':' + str(len(str(len(data_loader)))) + 'd' + '}/{1}]',
@@ -46,3 +49,34 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('{} Total time: {} ({:.4f} s / it)'.format(
         'Epoch: [{}]'.format(epoch), total_time_str, total_time / len(data_loader)))
+    return ave_loss.item() / len(data_loader)
+
+
+def evaluate(model: torch.nn.Module, criterion: torch.nn.Module, device: torch.device,
+             data_loader: Iterable, print_freq: int, scaler=None):
+    model.eval()
+    ave_loss = torch.tensor(0.).to(device)
+    with torch.no_grad(), torch.cuda.amp.autocast(enabled=scaler is not None):
+        for i, (image, target) in enumerate(data_loader):
+            image, target = image.to(device), target.to(device)
+            iter_time = time.time()
+            output = model(image)
+            loss = criterion(output, target)
+            ave_loss += loss
+
+            if i % print_freq == 0 or i == len(data_loader) - 1:
+                iter_time = time.time() - iter_time
+                log_msg = "; ".join([
+                    'Test:',
+                    '[{0' + ':' + str(len(str(len(data_loader)))) + 'd' + '}/{1}]',
+                    '{meters}',
+                    'time: {time}',
+                    'max mem: {memory:.0f}'
+                ])
+                if torch.cuda.is_available():
+                    print(log_msg.format(
+                        i, len(data_loader),
+                        meters=str(loss),
+                        time=str(iter_time),
+                        memory=torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)))
+    return ave_loss.item() / len(data_loader)
